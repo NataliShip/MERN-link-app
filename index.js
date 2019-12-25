368afbb8aa9958e2401eb0d9c27e72b13dd5bc44
Nataliya Shipelova <natulechka_m.25@mail.ru>
2019-12-25 03:17:19 +0300

    feat - setup mongodb connection

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const express = require('express')
 const config = require('config')
+const mongoose = require('mongoose')
 
 const server = express()
 
 const PORT = config.get('port') || 5000
 
-server.listen(PORT, () => console.log(`App has been started! on port ${PORT}`))
+async function start() { // wrap for Promise mongoose.connect()
+  try {
+    await mongoose.connect(config.get('mongoUri'), {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    })
+    server.listen(PORT, () => console.log(`App has been started! on port ${PORT}`))
+  } catch (e) {
+    console.log(`Server Error ${e.message}`)
+    process.exit(1) // exit Node.js
+  }
+}
+
+start()
